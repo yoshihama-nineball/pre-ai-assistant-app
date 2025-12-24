1689c05cc685765e0454ad5a5740bc2a54920775
yoshihama-nineball <burimofumofu@gmail.com>
2025-12-24 15:57:10 +0900

    fix: アプリ紹介

--- frontend/src/components/reframing/Reframing.tsx
+++ frontend/src/components/reframing/Reframing.tsx
@@ -71,6 +71,9 @@ const Reframing = () => {
       <Typography variant="h4" component="h1" gutterBottom align="center">
         🦍 Dr.ゴリのリフレーミング相談室「みかた」🔥
       </Typography>
+      <Typography variant="h6" component="h6" gutterBottom align="center">
+        ものごとの「見かた」を変えることのできるあなたの「味方」です
+      </Typography>
 
       {sessionInfo && (
         <Alert severity="info" sx={{ mb: 3 }}>
